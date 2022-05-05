f7a577a51e1b5a67351c1a612dd2da10a175cf23
chirag <99802739+chirag1132@users.noreply.github.com>
2022-05-05 13:11:18 +0530

    Add files via upload

---
+++ Loptop.js
@@ -0,0 +1,30 @@
+Status = "";
+Loptop_image = "";
+
+function preload(){
+    Loptop_image = loadImage("Loptop.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(Loptop_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(Loptop_image,0,0,640,350);
+}
